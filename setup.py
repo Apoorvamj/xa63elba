0049a3217956e97c43f6ec83b1c97fa64a5e50fb
Apoorvamj <93726574+Apoorvamj@users.noreply.github.com>
2021-11-13 22:23:43 +0530

    Create setup.py
    Generate a set-up to ensure the functioning of  package:

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(name='xa63elba',
+version='0.1',
+author='DSSS',
+author_email='apoorva.jayaprakash@fau.de',
+packages=find_packages(),
+install_requires=['numpy', 'Pillow', 'ipywidgets'])
